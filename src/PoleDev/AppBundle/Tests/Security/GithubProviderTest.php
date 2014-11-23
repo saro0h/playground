366fdf93a9ff4426a4160ded351838f542049fb2
Saro0h <mkhalil.sarah@gmail.com>
2014-11-23 14:41:32 +0100

    --

--- src/PoleDev/AppBundle/Tests/Security/GithubProviderTest.php
+++ src/PoleDev/AppBundle/Tests/Security/GithubProviderTest.php
@@ -69,6 +69,7 @@ class GithubProviderTest extends \PHPUnit_Framework_TestCase
 
     public function tearDown()
     {
+        $this->prophet->checkPredictions();
         $this->prophet = null;
     }
 }
